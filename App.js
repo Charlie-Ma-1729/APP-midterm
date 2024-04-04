0eb302f6bf3ee4592fdb26bbb5fb6550fade79a4
hugetuna <113012817+hugetuna@users.noreply.github.com>
2024-04-04 14:52:19 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+//規定一定要放最上面，不能亂動
+import 'react-native-gesture-handler';
+//---------------------------------------
+//
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+//引入導航
+import Navigator from './resource/navigator/navigator';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Navigator></Navigator>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
