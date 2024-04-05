333d4e86809d3841d2335c4b8fac4bd5993b8be6
Charlie-Ma-1729 <717charlie@gmail.com>
2024-04-05 16:12:08 +0800

    chart package

--- resource/Component/PerDeck/index.js
+++ resource/Component/PerDeck/index.js
@@ -1,14 +1,91 @@
-import * as React from 'react';
-import { Avatar, Button, Card, Text } from 'react-native-paper';
+import * as React from "react";
+import { Avatar, Button, Card, Text } from "react-native-paper";
 
+import { BarChart } from "react-native-gifted-charts";
+import { Image, View } from "react-native";
+import style from "./style.js";
+import { useTheme } from "react-native-paper";
 
-const PerDeck = () => (
-  <Card mode='outlined'>
-    <Card.Content>
-      <Text variant="titleLarge">殘機</Text>
-      <Text variant="bodyMedium">Card content</Text>
-    </Card.Content>
-  </Card>
-);
+const cardImg = require("../../../assets/images/Card.png");
+
+const PerDeck = () => {
+  const theme = useTheme();
+  const inlinestyle = {
+    Line: {
+      borderBottomColor: theme.colors.outlineVariant,
+      borderBottomWidth: 1,
+      marginVertical: 4,
+      marginHorizontal: 8,
+      fontSize: 1,
+    },
+    attr: {
+      marginHorizontal: 2,
+      flex: 1,
+      width: 1,
+      padding: 0,
+    },
+  };
+  const data = [
+    { value: 10, label: "0" },
+    { value: 4, label: "1" },
+    { value: 2, label: "2" },
+    { value: 2, label: "3" },
+    { value: 0, label: "4" },
+    { value: 2, label: "5+" },
+  ];
+  const yLabel = ["0", "2", "4", "6", "8", "10"];
+  return (
+    <Card mode="outlined" style={style.Card}>
+      <Card.Content>
+        <Text variant="titleLarge">殘機</Text>
+        <Text style={inlinestyle.Line} />
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          <Image source={cardImg} style={style.cover} />
+          <Card mode="outlined" style={style.ChartCard}>
+            <BarChart
+              data={data}
+              barWidth={12}
+              height={100}
+              xAxisLabelsVerticalShift={-8}
+              yAxisLabelWidth={30}
+              spacing={12}
+              maxValue={10}
+              dashGap={0}
+              noOfSections={5}
+              yAxisLabelTexts={yLabel}
+              formatYLabel={(yLabel) => {
+                const labelVal = Number(yLabel);
+                if (labelVal >= 10) return "10+";
+                return yLabel;
+              }}
+              style={{ flex: 1 }}
+            />
+            <View
+              style={{
+                flexDirection: "column",
+                justifyContent: "center",
+                flex: 0.1,
+                width: 1,
+              }}
+            >
+              <Button icon="camera" mode="contained" style={style.attr}>
+                10
+              </Button>
+              <Button icon="camera" mode="contained" style={style.attr}>
+                5
+              </Button>
+              <Button icon="camera" mode="contained" style={style.attr}>
+                0
+              </Button>
+              <Button icon="camera" mode="contained" style={style.attr}>
+                5
+              </Button>
+            </View>
+          </Card>
+        </View>
+      </Card.Content>
+    </Card>
+  );
+};
 
 export default PerDeck;
---
+++ resource/Component/PerDeck/style.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+  Card: {
+    margin: 16,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+  },
+  cover: {
+    width: 22,
+    flex: 0.65,
+  },
+  ChartCard: {
+    paddingVertical: 8,
+    paddingHorizontal: 4,
+    height: 150,
+    borderRadius: 10,
+    backgroundColor: "#fff",
+    flex: 2,
+    flexDirection: "row",
+    alignItems: "right",
+  },
+});
