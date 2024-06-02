73fa065f82563231fd490f4a56384771001e3454
hugetuna <q22589761@gmail.com>
2024-06-02 16:58:53 +0800

    deckListSlice

---
+++ resource/redux/deckListSlice.js
@@ -0,0 +1,33 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { deleteFile } from 'realm';
+//此變數用於篩選卡牌頁卡片
+//定義起始變數(state)
+const initialState = {
+    deckList: []
+};
+
+const deckListSlice = createSlice({
+    name: 'deckList',
+    initialState,
+    //定義函式(action)
+    reducers: {
+        addDeck: (state, action) => {
+            state.deckList.push(action.payload);
+        },
+        deleteDeck: (state, action) => {
+            const tar = action.payload;
+            state.deckList = state.deckList.filter((item) => {
+                return (item !== tar)
+            })
+        }
+    },
+});
+
+// export state to global
+export const selectDeckList = (state) => state.deckList.deckList;
+
+// export actions to global
+export const { addDeck, deleteDeck } = deckListSlice.actions;
+
+// export reducer to global
+export default deckListSlice.reducer;
