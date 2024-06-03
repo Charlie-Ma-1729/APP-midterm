0d3dad20072e2a73498cd68d6afb270c22a4f532
Charlie-Ma-1729 <717charlie@gmail.com>
2024-06-03 13:39:45 +0800

    editing mode done

--- resource/screen/DeckInsideScreen.js
+++ resource/screen/DeckInsideScreen.js
@@ -1,5 +1,16 @@
+import * as React from "react";
 //react-native-paper提供的物件
-import { IconButton, Appbar, useTheme, FAB } from "react-native-paper";
+import {
+  IconButton,
+  Appbar,
+  useTheme,
+  FAB,
+  Portal,
+  Dialog,
+  TextInput,
+  Card,
+  Button,
+} from "react-native-paper";
 
 //普通宣告
 import {
@@ -8,7 +19,6 @@ import {
   View,
   StatusBar,
   ScrollView,
-  Button,
 } from "react-native";
 
 import DeckIn from "../Component/DeckIn";
@@ -23,16 +33,29 @@ import {
   selectEditingDeckId,
 } from "../redux/isEditSlice.js";
 
-const DeckInsideScreen = ({ navigation ,route}) => {
-    const currentDeckId = route.params.id;
+const DeckInsideScreen = ({ navigation, route }) => {
+  const currentDeckId = route.params.id;
+  const currentDeckName = route.params.name;
+  const [visible, setVisible] = React.useState(false);
   const theme = useTheme();
   const isEdit = useSelector(selectIsEdit);
   const editingDeckId = useSelector(selectEditingDeckId);
   const dispatch = useDispatch();
+  const showDialog = () => setVisible(true);
+  const hideDialog = () => {
+    setVisible(false);
+  };
+  const choosetoEditthis = () => {
+    dispatch(editOn());
+    dispatch(setEditingDeckId(currentDeckId));
+    dispatch(setEditingDeck(currentDeckName));
+  };
   return (
     <View
       style={{ ...styles.container, backgroundColor: theme.colors.surface }}
-    >
+    >   
+    {!isEdit && (
+    <>
       <FAB
         color={theme.colors.onPrimaryContainer}
         backgroundColor={theme.colors.primaryContainer}
@@ -40,11 +63,73 @@ const DeckInsideScreen = ({ navigation ,route}) => {
         icon="pencil"
         label="編輯模式"
         onPress={() => {
-          dispatch(editOn());
-          dispatch(setEditingDeckId(currentDeckId));
+          if (isEdit && editingDeckId !== currentDeckId) {
+            showDialog();
+          } else {
+            choosetoEditthis();
+          }
         }}
       />
+      </>
+      )}
+      {isEdit && editingDeckId != currentDeckId && (
+    <>
+      <FAB
+        color={theme.colors.onPrimaryContainer}
+        backgroundColor={theme.colors.primaryContainer}
+        style={styles.FABStyle}
+        icon="pencil"
+        label="編輯模式"
+        onPress={() => {
+          if (isEdit && editingDeckId !== currentDeckId) {
+            showDialog();
+          } else {
+            choosetoEditthis();
+          }
+        }}
+      />
+      </>
+      )}
+      {isEdit && editingDeckId == currentDeckId && (
+    <>
+      <FAB
+        color={theme.colors.onPrimaryContainer}
+        backgroundColor={theme.colors.primaryContainer}
+        style={styles.FABStyle}
+        icon="content-save"
+        label="結束編輯"
+        onPress={() => {
+            dispatch(editOff());
+        }}
+      />
+      </>
+      )}
       <DeckIn currentDeckId={currentDeckId} />
+      <Portal>
+        <Dialog visible={visible} onDismiss={hideDialog}>
+          <Dialog.Title>正在編輯其他牌組</Dialog.Title>
+          <Dialog.Content>
+            <Text>是否結束編輯，並改為編輯本牌組？</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+          <Button
+              onPress={() => {
+                hideDialog();
+              }}
+            >
+              取消
+            </Button>
+            <Button
+              onPress={() => {
+                hideDialog();
+                choosetoEditthis();
+              }}
+            >
+              確定
+            </Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
       <StatusBar style="auto" />
     </View>
   );
