dd458c9c90e59d71b479ae184432b03d91fa74ae
Charlie-Ma-1729 <717charlie@gmail.com>
2024-06-04 00:32:33 +0800

    screw you barchart

--- resource/Component/PerDeck/index.js
+++ resource/Component/PerDeck/index.js
@@ -8,98 +8,27 @@ import { Image, View } from "react-native";
 import style from "./style.js";
 import { useTheme } from "react-native-paper";
 
-
 const PerDeck = ({ name, id, picture }) => {
   const theme = useTheme();
   const navigation = useNavigation();
-  const inlinestyle = {
-    Line: {
-      borderBottomColor: theme.colors.outlineVariant,
-      borderBottomWidth: 1,
-      marginVertical: 4,
-      marginHorizontal: 8,
-      fontSize: 1,
-    },
-    attr: {
-      marginHorizontal: 2,
-      padding: 0,
-      backgroundColor: theme.colors.primaryContainer,
-      color: theme.colors.onPrimaryContainer,
-      icon: { color: theme.colors.onPrimaryContainer },
-    },
-  };
-  const data = [
-    { value: 10, label: "0" },
-    { value: 4, label: "1" },
-    { value: 2, label: "2" },
-    { value: 2, label: "3" },
-    { value: 0, label: "4" },
-    { value: 2, label: "5+" },
-  ];
-  const yLabel = ["0", "2", "4", "6", "8", "10"];
   return (
     <Card
       mode="outlined"
       style={[style.Card, { backgroundColor: theme.colors.surface }]}
-      onPress={() => navigation.navigate("牌組詳細",{id:id, name:name})}
+      onPress={() => navigation.navigate("牌組詳細", { id: id, name: name })}
     >
       <Card.Content>
-        <Text
-          variant="titleLarge"
-          style={{ backgroundColor: theme.colors.onsurface }}
-        >
-          {name}
-        </Text>
-        <Text style={inlinestyle.Line} />
         <View style={style.container}>
           <View style={style.leftContainer}>
             <Image source={{ uri: picture }} style={style.cover} />
           </View>
           <View style={style.rightContainer}>
-            <Card mode="outlined" style={[style.ChartCard, { borderColor: theme.colors.surfaceVariant }]}>
-              <View style={style.chart}>
-                <BarChart
-                  data={data}
-                  barWidth={10}
-                  height={80}
-                  xAxisLabelsVerticalShift={-8}
-                  yAxisLabelWidth={30}
-                  spacing={20}
-                  maxValue={10}
-                  dashGap={0}
-                  noOfSections={5}
-                  yAxisLabelTexts={yLabel}
-                  formatYLabel={(yLabel) => {
-                    const labelVal = Number(yLabel);
-                    if (labelVal >= 10) return "10+";
-                    return yLabel;
-                  }}
-                  style={style.barchart}
-                  frontColor={theme.colors.primary}
-                  backgroundColor={theme.colors.surface}
-                  xAxisLabelTextStyle={{ color: theme.colors.primary }}
-                  yAxisTextStyle={{ color: theme.colors.primary }}
-                />
-              </View>
-              {/* <View style={style.buts}>
-                <Button
-                  icon="camera"
-                  mode="contained"
-                  sizestyle={inlinestyle.attr}
-                >
-                  1000
-                </Button>
-                <Button icon="camera" mode="contained" style={inlinestyle.attr}>
-                  5
-                </Button>
-                <Button icon="camera" mode="contained" style={inlinestyle.attr}>
-                  0
-                </Button>
-                <Button icon="camera" mode="contained" style={inlinestyle.attr}>
-                  5
-                </Button>
-              </View> */}
-            </Card>
+            <Text
+              variant="titleLarge"
+              style={{ backgroundColor: theme.colors.onsurface }}
+            >
+              {name}
+            </Text>
           </View>
         </View>
       </Card.Content>
